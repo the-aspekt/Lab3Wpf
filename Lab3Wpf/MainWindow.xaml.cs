876044098b8daea6855cedb828275a8c7745931f
Pavel <sozonovps@gmail.com>
2024-03-22 13:00:24 +0800

    Добавьте файлы проекта.

---
+++ Lab3Wpf/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab3Wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                string selectedFont = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+                textBox.FontFamily = new FontFamily(selectedFont);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                int selectedFontSize = Convert.ToInt16(((sender as ComboBox).SelectedItem as TextBlock).Text);
+                textBox.FontSize = selectedFontSize;
+            }
+        }
+
+        private void Image_IsEnabledChanged(object sender, MouseButtonEventArgs e)
+        {
+            if (textBox.FontWeight != FontWeights.Bold)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+            }
+            else
+            {
+                textBox.FontWeight = FontWeights.Normal;
+            }
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (textBox.FontStyle != FontStyles.Italic)
+            {
+                textBox.FontStyle = FontStyles.Italic;
+            }
+            else
+            {
+                textBox.FontStyle = FontStyles.Normal;
+            }
+        }
+
+        private void ToggleButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (textBox.TextDecorations.Count == 0)
+            {
+                textBox.TextDecorations.Add(TextDecorations.Underline);
+            }
+            else
+            {
+                textBox.TextDecorations.Clear();
+            }
+           
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = new SolidColorBrush(Colors.Red);
+            }
+        }
+    }
+}
